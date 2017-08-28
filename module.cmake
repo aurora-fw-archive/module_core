15f1a60f48b7e6483041c92f4c8e63ee65a43091
Luís Ferreira <lsferreira169@gmail.com>
2017-08-28 21:09:31 +0100

    Add install feature in the build system

--- module.cmake
+++ module.cmake
@@ -5,6 +5,7 @@ if (NOT CONFIGURED_ONCE)
 endif()
 
 include_directories(${MODULE_CORE_DIR}/include)
+file(GLOB MODULE_CORE_HEADERS_CORE ${MODULE_CORE_DIR}/include/Aurora/Core/*.h)
 
 add_library (aurorafw-core SHARED ${MODULE_CORE_SOURCE_DIR}/Debug.cpp
                               	  ${MODULE_CORE_SOURCE_DIR}/Application.cpp)
@@ -12,3 +13,6 @@ add_library (aurorafw-core SHARED ${MODULE_CORE_SOURCE_DIR}/Debug.cpp
 target_link_libraries(aurorafw-core aurorafw-cli)
 
 set_target_properties(aurorafw-core PROPERTIES OUTPUT_NAME aurorafw-core)
+
+install(TARGETS aurorafw-core DESTINATION lib)
+install(FILES ${MODULE_CORE_HEADERS_CORE} DESTINATION include/Aurora/Core)
